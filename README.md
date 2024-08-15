a7d809ed850e269e4b8efd477c26b617a16af271
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-08-15 10:11:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# scic-job-task-server
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivo4yuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      await client.connect();
+
+      app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+      console.log("You successfully connected to MongoDB!");
+    } finally {
+
+    }
+  }
+  run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
